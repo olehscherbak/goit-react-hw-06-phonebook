7e29ddaf1342a63142fc9fcee7c6dd2024e9fd43
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-02-26 22:44:35 +0200

    added redux-persist

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,23 +2,7 @@ import { createReducer } from '@reduxjs/toolkit';
 
 import { addContact, deleteContact, filterChange } from './actions';
 
-const contactsInitialState = [
-  {
-    id: 'id-1',
-    name: 'Oleh Scherbak',
-    number: '+38067-247-62-45',
-  },
-  {
-    id: 'id-2',
-    name: 'office',
-    number: '+38044-545-62-80',
-  },
-  {
-    id: 'id-3',
-    name: 'home',
-    number: '+38099-314-85-45',
-  },
-];
+const contactsInitialState = [];
 
 const filterInitialState = '';
 
